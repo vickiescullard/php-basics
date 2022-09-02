51f0f0f43c9c6d76ae02fd676194b9a8cba707bb
Vickie Scullard <vickie.scullard@gmail.com>
2022-09-02 17:12:42 +0100

    php maths doc completed

---
+++ maths.php
@@ -0,0 +1,19 @@
+<?php
+
+#absolute number
+echo abs(-4.2) . "<br><br>";
+
+# highest integer
+echo ceil(0.70) . "<br><br>";
+
+# max
+echo(max(2,4,6,8,10) . "<br><br>");
+
+# min
+echo(min(-5, 3, 8, 15, 100) . "<br><br>");
+
+# random
+echo(rand() . "<br><br>");
+echo(rand(10,20) . "<br><br>");
+
+?>
