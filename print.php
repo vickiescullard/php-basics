ce8b747d4b8747aa376315d5b8cf316376bffd1f
Vickie Scullard <vickie.scullard@gmail.com>
2022-09-02 17:13:11 +0100

    php print doc completed

---
+++ print.php
@@ -0,0 +1,17 @@
+<?php
+
+#echo
+$helpMe = "Help me";
+echo $helpMe;
+echo "<br><br>";
+
+#print
+print "Hello world";
+echo "<br><br>";
+
+#print_r
+$vegetables = array("veg1"=>"carrots", "veg2"=>"onions");
+print_r ($vegetables);
+echo "<br><br>";
+
+?>
